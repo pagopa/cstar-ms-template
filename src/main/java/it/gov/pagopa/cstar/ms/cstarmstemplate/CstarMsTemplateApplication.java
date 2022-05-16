bd1d12b8c71e0de9455ca663c2ecc8bf8f93e0ad
TommasoLencioni <tommaso.lencioni@pagopa.it>
2022-05-16 12:25:51 +0200

    [RTD-92] add main

---
+++ src/main/java/it/gov/pagopa/cstar/ms/cstarmstemplate/CstarMsTemplateApplication.java
@@ -0,0 +1,13 @@
+package it.gov.pagopa.cstar.ms.cstarmstemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CstarMsTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CstarMsTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/gov/pagopa/cstar/ms/cstarmstemplate/CstarMsTemplateApplicationTests.java
@@ -0,0 +1,16 @@
+package it.gov.pagopa.cstar.ms.cstarmstemplate;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CstarMsTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertTrue(true);
+	}
+
+}
